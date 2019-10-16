72e1ce7dd1e2b063626ce79a01b07fe1d91ee693
Leonardo Santana <leonardoluis.santana@hotmail.com>
2019-10-16 15:38:26 -0300

    first commit

---
+++ Models/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace CadPessoa.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+
+        //public System.Data.Entity.DbSet<CadPessoa.Models.Pessoa> Pessoas { get; set; }
+    }
+}
---
+++ Models/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CadPessoa.Models
+{
+    public class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode =true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DataType(DataType.Date, ErrorMessage ="Formato invalido")]
+        public DateTime DtNascimento { get; set; }
+    }
+}
